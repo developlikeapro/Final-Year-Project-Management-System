3912fb365403f3d89b78825c9247aace4a20c0db
Izma Khalid Qureshi <izmaqureshi@gmail.com>
2017-04-28 14:58:58 +0500

    FYP-Master-Student Update
    Student updated

---
+++ FYP-master/src/fyp_management_system/FYP_Management_System.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fyp_management_system;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author M GHOUS
+ */
+public class FYP_Management_System {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static Ccontroler Ocontroler=null;
+    
+    public static void main(String[] args) {
+        // TODO code application logic 
+    //static public Ccontroler Ocontroler=null;
+    
+       		System.out.print("Condition:\n");
+                System.out.println("Running.....:");
+            
+    }
+    
+}
---
+++ FYP-master/src/fyp_management_system/Iadvisor.java
@@ -0,0 +1,8 @@
+package fyp_management_system;
+
+public interface Iadvisor
+{
+      void checking_proposed_idea(int studid);
+     void provide_recomendation(int studid);
+       void view_project(int studid);
+}
